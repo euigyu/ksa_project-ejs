6efc67e9e4a671c0a4c815db58359a0717cff12e
pumpkinjeon <wkghd951223@gmail.com>
2021-04-09 17:25:55 +0900

    실서버용 IP 변경

--- public/js/board_new.js
+++ public/js/board_new.js
@@ -32,7 +32,7 @@ function addFiles(e) {
   }
 
   $.ajax2({
-    url: "http://localhost:3008/api/file",
+    url: "http://3.35.50.200:3008/api/file",
     processData: false,
     contentType: false,
     enctype: "multipart/form-data",
@@ -86,7 +86,7 @@ function uploadFile() {
   }
 
   $.ajax2({
-    url: `http://localhost:3008/api/board/${subject}`,
+    url: `http://3.35.50.200:3008/api/board/${subject}`,
     // url: `http://3.35.50.200:3008/api/board/${subject}`,
     processData: false,
     contentType: "application/json",
@@ -94,7 +94,7 @@ function uploadFile() {
     type: "POST",
     success: function (result) {
       // location.href = `http://3.35.50.200:3008/board/${subject}`
-      location.href = `http://localhost:3008/board/${subject}`
+      location.href = `http://3.35.50.200:3008/board/${subject}`
     },
   });
 }
