12f6eba1f9d17b32a3d550f3483b047668897a89
euigyu <afterscool926@gmail.com>
2021-04-12 18:01:00 +0900

    localhost => remote ip

--- public/js/onlinetest_input.js
+++ public/js/onlinetest_input.js
@@ -15,13 +15,13 @@ function addQuestionListener (e) {
 }
 function addQuestion(idx) {
   $.ajax2({
-    url: `http://localhost:3008/api/onlineTest/list/${subject}`,
+    url: `http://3.35.50.200:3008/api/onlineTest/list/${subject}`,
     processData: false,
     contentType: "application/json",
     type: "GET",
     success: function (_result) {
       $.ajax2({
-        url: `http://localhost:3008/api/onlineTest/multipleChoiceList/${subject}`,
+        url: `http://3.35.50.200:3008/api/onlineTest/multipleChoiceList/${subject}`,
         processData: false,
         contentType: "application/json",
         type: "GET",
@@ -177,7 +177,7 @@ $('.register').on('click', function (e) {
   // ajax post 통신
   $(arr).each(function () {
     $.ajax2({
-      url: `http://localhost:3008/api/onlineTest/${subject}/testinput`,
+      url: `http://3.35.50.200:3008/api/onlineTest/${subject}/testinput`,
       // dataType: 'json',
       contentType: "application/json",
       type: this.q_no ? 'PUT' : 'POST',
